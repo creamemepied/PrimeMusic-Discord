6ae55dba656aff4fe2d64f21969e8e5ad071d6eb
Aryan Pandey <creamemepied@gmail.com>
2025-08-06 17:34:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,16 +3,16 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  ownerID: ["1397227302462427230", ""], 
+  mongodbUri : "mongodb+srv://shiva:shiva@discordbot.opd5w.mongodb.net/?retryWrites=true&w=majority",
+  spotifyClientId : "f71a3da30e254962965ca2a89d6f74b9",
+  spotifyClientSecret : "199a619d22dd4e55a4a2c1a7a3d70e63",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/55GhT9N6Cq",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
